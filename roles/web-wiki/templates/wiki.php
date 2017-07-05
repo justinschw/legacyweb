7f82b51aab3d3db8e878d73a1f3360e495ead426
Justin Schwartzbeck <justin@legacyweb.legacyweb.net>
2017-07-05 07:26:30 -0500

    Fixed bug with wikipedia; added images. Still need to do more formatting

--- roles/web-wiki/templates/wiki.php
+++ roles/web-wiki/templates/wiki.php
@@ -1,9 +1,3 @@
-<?php
-   // load Zend classes
-   require_once 'Zend/Loader.php';
-   Zend_Loader::loadClass('Zend_Rest_Client');
-   ?>
-
 <html>
   <head>
     <title>Legacyweb - Wikipedia</title>
@@ -21,6 +15,7 @@
 	      <?php
 		 if(isset($_GET["q"]) && $_GET["q"] != "") {
 		   $query = $_GET["q"];
+		   $query = urlencode($query);
 		   $query_url="https://en.wikipedia.org/w/api.php?action=query&list=search&srwhat=text&format=json&srsearch=" . $query;
 	           // perform request
                    $query_result = file_get_contents($query_url);
@@ -49,12 +44,32 @@
 	    <?php
 	       if(isset($_GET["display"]) && $_GET["display"] == "showPage") {
 	         $wikipage = $_GET["wikipage"];
-		 $query_url="https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=".$wikipage."&redirects=";
+		 $query_url="https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=".urlencode($wikipage)."&redirects=";
+		 $query_image_url="https://en.wikipedia.org/w/api.php?action=query&titles=".urlencode($wikipage)."&prop=pageimages&format=json&pithumbsize=150";
                  $query_result = file_get_contents($query_url);
+		 $query_image_result=file_get_contents($query_image_url);
                  $result_json = json_decode($query_result);
+		 $result_image_json = json_decode($query_image_result);
+		 print("<table>");
+		 print("<tr>");
+		 print("<td bgcolor=#99a3a4>");
+		 print("<h2>".$wikipage."</h2>");
+		 print("</td>");
+		 print("<td width=150>");
+		 foreach($result_image_json->{"query"}->{"pages"} as $page) {
+		   if (isset($page->{'thumbnail'})) {
+		     file_put_contents("/var/www/html/images-tmp/pageimg", file_get_contents($page->{"thumbnail"}->{"source"}));
+		     print("<img src='/images-tmp/pageimg'>");
+		   }
+		 }
+		 print("</td>");
+		 print("<tr>");
+		 print("<td colspan=2>");
                  foreach($result_json->{"query"}->{"pages"} as $page) {
 		   print($page->{"extract"});
 		 }
+		 print("</td>");
+		 print("</table>");
 	       }
 	       ?>
 	  </td>
